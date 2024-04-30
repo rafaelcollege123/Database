5593e95824b157e5407b12c40c767e409178ce0c
Rafael FIgueroa <rafaelcollege123@gmail.com>
2024-04-29 20:03:20 -0700

    New location window

--- Databasefinal2/IMS/IMS_APP/models.py
+++ Databasefinal2/IMS/IMS_APP/models.py
@@ -5,7 +5,11 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     quantity = models.DecimalField(max_digits=5, decimal_places=0 , default = 0)
-    location = models.CharField(max_length=100, null=True)
+    locationId = models.CharField(max_length=100, null=True)
+    location_state = models.CharField(max_length = 100, null=True)
+    location_city = models.CharField(max_length = 100, null=True)
+
+
 
     def __str__(self):
         return self.name
--- Databasefinal2/IMS/IMS_APP/views.py
+++ Databasefinal2/IMS/IMS_APP/views.py
@@ -5,13 +5,20 @@ def product_list(request):
     products = Product.objects.all()
     return render(request, 'IMS_APP/product_list.html', {'products': products})
 
+
+# ADD Location ID
+#ProcuctID , size  
 def add_product(request):
     if request.method == 'POST':
         name = request.POST['name']
         description = request.POST['description']
         price = request.POST['price']
         quantity = request.POST['quantity']
-        location = request.POST['location']
-        Product.objects.create(name=name, description=description, price=price, quantity=quantity, location = location)
+        locationId = request.POST['locationId']
+        Product.objects.create(name=name, description=description, price=price, quantity=quantity, location = locationId)
         return redirect('product_list')
     return render(request, 'IMS_APP/add_product.html')
+
+def location_info(request):
+    products = Product.objects.all()
+    return render(request, 'IMS_APP/location_info.html', {'products': products})
